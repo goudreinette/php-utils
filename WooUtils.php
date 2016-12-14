8722a061aff13044e2d3a597839ddcb7c5bcd625
reinvdwoerd <reinvanderwoerd@me.com>
2016-12-14 11:15:20 +0100

    include child categories

--- WooUtils.php
+++ WooUtils.php
@@ -6,9 +6,9 @@ class WooUtils
      * @param array $args Additional query arguments
      * @return Array [String] of product categories
      */
-    function getProductCategories($args = [])
+    static function getProductCategories($args = [])
     {
-        $categories = get_categories(array_merge(['taxonomy' => 'product_cat'], $args));
+        $categories = get_categories(array_merge(['taxonomy' => 'product_cat', 'hierarchical' => true], $args));
         return array_values(Utils::array_pluck($categories, 'cat_name'));
     }
 
@@ -17,7 +17,7 @@ class WooUtils
      * @param $time String
      * @return String date formatted to WooCommerce preferences
      */
-    function formatDateTimeWoocommerce($date, $time)
+    static function formatDateTimeWoocommerce($date, $time)
     {
         $formatted = date_i18n(wc_date_format(), strtotime($date));
         return "$formatted, $time uur";
@@ -28,11 +28,23 @@ class WooUtils
      * @param \WC_Product $product
      * @return string
      */
-    function featuredText(\WC_Product $product)
+    static function featuredText(\WC_Product $product)
     {
         if ($product->is_featured())
             return "featured";
         if ($product->is_on_sale())
             return "sale";
+    }
+
+    static function flattenMeta($ids, $metaKey)
+    {
+        foreach ($ids as $id) {
+            $meta = get_post_meta($id, $metaKey, true);
+
+            foreach ($meta as $subKey => $subValue) {
+                $fullKey = "$metaKey-$subKey";
+                update_post_meta($id, $fullKey, $subValue);
+            }
+        }
     }
 }
